c1f8ead1e10784040ad7d7dbdcf204fea3f92d39
vishu-tyagi <vishweshwartyagiv@gmail.com>
2023-01-13 10:31:49 +0000

    add package

---
+++ src/nyc-taxi/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name="nyc_taxi",
+    version="0.0.1",
+    description="Package for NYC TLC database",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.9"
+)
